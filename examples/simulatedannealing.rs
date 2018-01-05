0e6c2bbb9ad4f9ce8e7035e8040e18a4e1e4b90f
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-01-05 23:13:04 +0100

    better error handling in example

--- examples/simulatedannealing.rs
+++ examples/simulatedannealing.rs
@@ -3,7 +3,7 @@ use argmin::problem::Problem;
 use argmin::sa::{SATempFunc, SimulatedAnnealing};
 use argmin::testfunctions::rosenbrock;
 
-fn main() {
+fn run() -> Result<(), Box<std::error::Error>> {
     // Define cost function
     let cost = |x: &Vec<f64>| -> f64 { rosenbrock(x, 1_f64, 100_f64).unwrap() };
 
@@ -15,14 +15,21 @@ fn main() {
     let prob = Problem::new(&cost, &lower_bound, &upper_bound);
 
     // Set up simulated annealing solver
-    let mut solver = SimulatedAnnealing::new(10.0, 10_000_000).unwrap();
+    let mut solver = SimulatedAnnealing::new(10.0, 10_000_000)?;
     solver.temp_func(SATempFunc::Exponential(0.8));
 
     // definie inital parameter vector
     // let init_param: Vec<f64> = vec![-1.0, 2.0];
 
-    let result = solver.run(&prob, &prob.random_param().unwrap()).unwrap();
+    let result = solver.run(&prob, &prob.random_param()?)?;
 
     // print result
     println!("{:?}", result);
+    Ok(())
+}
+
+fn main() {
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
+    }
 }
