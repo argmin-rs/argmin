efbffd2c5a5af9248f6100306d4b9409fca09dca
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-01-14 20:29:32 +0100

    updated nelder mead example

--- examples/neldermead.rs
+++ examples/neldermead.rs
@@ -7,11 +7,10 @@ use argmin::testfunctions::{rosenbrock, rosenbrock_derivative, sphere, sphere_de
 fn run() -> Result<(), Box<std::error::Error>> {
     // Define cost function
     let cost = |x: &Vec<f64>| -> f64 { rosenbrock(x, 1_f64, 100_f64).unwrap() };
-    // let gradient = |x: &Vec<f64>| -> Vec<f64> { rosenbrock_derivative(x, 1_f64, 100_f64).unwrap() };
     // let cost = |x: &Vec<f64>| -> f64 { sphere(x).unwrap() };
-    // let gradient = |x: &Vec<f64>| -> Vec<f64> { sphere_derivative(x).unwrap() };
 
     // Define bounds
+    // Bounds are not satisfied yet.
     let lower_bound: Vec<f64> = vec![-1.5, -0.5];
     let upper_bound: Vec<f64> = vec![2.0, 3.0];
 
@@ -20,9 +19,10 @@ fn run() -> Result<(), Box<std::error::Error>> {
 
     // Set up GradientDecent solver
     let mut solver = NelderMead::new();
-    // solver.max_iters(50);
     solver.max_iters(100);
 
+    // Choose the starting points.
+    // Randomly chosen starting points may make Nelder Mead converge very slowly.
     // let init_params = vec![
     //     prob.random_param()?,
     //     prob.random_param()?,
@@ -30,7 +30,6 @@ fn run() -> Result<(), Box<std::error::Error>> {
     // ];
     let init_params = vec![vec![0.0, 0.1], vec![2.0, 1.5], vec![2.0, -1.0]];
 
-    // solver.init(&prob)?;
     let result = solver.run(&prob, &init_params)?;
 
     // print result
