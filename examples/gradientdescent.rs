ce6e26c982dab040b391577936d21b9bb5782865
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-01-14 13:07:59 +0100

    more documentation for gradient descent example

--- examples/gradientdescent.rs
+++ examples/gradientdescent.rs
@@ -7,55 +7,74 @@ use argmin::backtracking::BacktrackingLineSearch;
 
 fn run() -> Result<(), Box<std::error::Error>> {
     // Define cost function
+    // Choose either `Rosenbrock` or `Sphere` function.
     let cost = |x: &Vec<f64>| -> f64 { rosenbrock(x, 1_f64, 100_f64).unwrap() };
     let gradient = |x: &Vec<f64>| -> Vec<f64> { rosenbrock_derivative(x, 1_f64, 100_f64).unwrap() };
     // let cost = |x: &Vec<f64>| -> f64 { sphere(x).unwrap() };
     // let gradient = |x: &Vec<f64>| -> Vec<f64> { sphere_derivative(x).unwrap() };
 
     // Define bounds
-    let lower_bound: Vec<f64> = vec![-1.5, -0.5];
-    let upper_bound: Vec<f64> = vec![2.0, 3.0];
+    // Note: Gradient Descent currently does not enforce these bounds which is why we can set them
+    // to -1000 and +1000
+    let lower_bound: Vec<f64> = vec![-1000.0, -1000.0];
+    let upper_bound: Vec<f64> = vec![1000.0, 1000.0];
+    // Unfortunately, setting them to +/- Infinity does not work (yet)
+    // let lower_bound: Vec<f64> = vec![std::f64::NEG_INFINITY, std::f64::NEG_INFINITY];
+    // let upper_bound: Vec<f64> = vec![std::f64::INFINITY, std::f64::INFINITY];
 
     // Set up problem
+    // The problem requires a cost function, lower and upper bounds and takes an optional gradient.
     let mut prob = Problem::new(&cost, &lower_bound, &upper_bound);
     prob.gradient(&gradient);
 
     // Set up GradientDecent solver
     let mut solver = GradientDescent::new();
+    // Set the maximum number of iterations to 10000
     solver.max_iters(10_000);
+    // Choose the method which calculates the step width. `GDGammaUpdate::Constant(0.0001)` sets a
+    // constant step width of 0.0001 while `GDGammaUpdate::BarzilaiBorwein` updates the step width
+    // according to TODO
     // solver.gamma_update(GDGammaUpdate::Constant(0.0001));
     solver.gamma_update(GDGammaUpdate::BarzilaiBorwein);
 
     // define inital parameter vector
-    let init_param: Vec<f64> = vec![1.5, 1.5];
+    // `Problem` allows to create random parameter vectors which satisfies `lower_bound` and
+    // `upper_bound`.
+    let init_param: Vec<f64> = prob.random_param()?;
+    // let init_param: Vec<f64> = vec![1.5, 1.5];
+    println!("{:?}", init_param);
 
-    // let result = solver.run(&prob, &prob.random_param()?)?;
+    // Actually run the solver on the problem.
     let result1 = solver.run(&prob, &init_param)?;
 
     // print result
     println!("{:?}", result1);
 
+    // Define new solver
     let mut solver = GradientDescent::new();
     solver.max_iters(10_000);
 
+    // Initialize a backtracking line search method to use for the gamma update method
     let mut linesearch = BacktrackingLineSearch::new(&cost, &gradient);
     linesearch.alpha(1.0);
-
     solver.gamma_update(GDGammaUpdate::BacktrackingLineSearch(linesearch));
 
-    // let result = solver.run(&prob, &prob.random_param()?)?;
+    // Run solver
     let result2 = solver.run(&prob, &init_param)?;
 
     // print result
     println!("{:?}", result2);
 
     // Manually solve it
+    // `GradientDescent` also allows you to initialize the solver yourself and run each iteration
+    // manually. This is particularly useful if you need to get intermediate results or if your
+    // need to print out information that is otherwise not accessible to you.
     let mut solver = GradientDescent::new();
     solver.init(&prob, &init_param)?;
 
     loop {
         let par = solver.next_iter();
-        println!("{:?}", par);
+        // println!("{:?}", par);
         if par.1 >= result1.iters {
             break;
         };
