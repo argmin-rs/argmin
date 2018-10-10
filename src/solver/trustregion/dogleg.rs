354741ff20c22389d6f4c174537d6a8495d61f6f
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-10-10 16:35:52 +0200

    basic structure for dogleg method, not implemented yet

---
+++ src/solver/trustregion/dogleg.rs
@@ -0,0 +1,141 @@
+// Copyright 2018 Stefan Kroboth
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+//! # Dogleg method
+//!
+//!
+//! ## Reference
+//!
+//! TODO
+//!
+// //!
+// //! # Example
+// //!
+// //! ```rust
+// //! todo
+// //! ```
+
+use prelude::*;
+use std;
+
+/// Dogleg method
+#[derive(ArgminSolver)]
+pub struct Dogleg<'a, T, H>
+where
+    T: Clone
+        + std::default::Default
+        + std::fmt::Debug
+        + ArgminWeightedDot<T, f64, H>
+        + ArgminNorm<f64>
+        + ArgminScale<f64>,
+    H: Clone + std::default::Default,
+{
+    /// Radius
+    radius: f64,
+    /// base
+    base: ArgminBase<'a, T, f64, H>,
+}
+
+impl<'a, T, H> Dogleg<'a, T, H>
+where
+    T: Clone
+        + std::default::Default
+        + std::fmt::Debug
+        + ArgminWeightedDot<T, f64, H>
+        + ArgminNorm<f64>
+        + ArgminScale<f64>,
+    H: Clone + std::default::Default,
+{
+    /// Constructor
+    ///
+    /// Parameters:
+    ///
+    /// `operator`: operator
+    pub fn new(
+        operator: Box<ArgminOperator<Parameters = T, OperatorOutput = f64, Hessian = H> + 'a>,
+    ) -> Self {
+        let base = ArgminBase::new(operator, T::default());
+        Dogleg {
+            radius: std::f64::NAN,
+            base: base,
+        }
+    }
+}
+
+impl<'a, T, H> ArgminNextIter for Dogleg<'a, T, H>
+where
+    T: Clone
+        + std::default::Default
+        + std::fmt::Debug
+        + ArgminWeightedDot<T, f64, H>
+        + ArgminNorm<f64>
+        + ArgminScale<f64>,
+    H: Clone + std::default::Default,
+{
+    type Parameters = T;
+    type OperatorOutput = f64;
+    type Hessian = H;
+
+    fn init(&mut self) -> Result<(), Error> {
+        self.base.reset();
+        // This is not an iterative method.
+        self.set_max_iters(1);
+        Ok(())
+    }
+
+    fn next_iter(&mut self) -> Result<ArgminIterationData<Self::Parameters>, Error> {
+        // TODO: Math trait ArgminInverse
+        //       Probably will only work with ndarray or nalgebra...
+        // compute tau
+        // pb = -H^-1g
+        // pu = - (g^Tg)/(g^THg) * g
+        // if 0 <= tau < 1
+        //     p* = tau * pu
+        // if 1 <= tau <= 2
+        //     p* = pu + (tau-1)*(pb - pu)
+        unimplemented!()
+        // let grad = self.base.cur_grad();
+        // let grad_norm = grad.norm();
+        // let wdp = grad.weighted_dot(self.base.cur_hessian().clone(), grad.clone());
+        // let tau: f64 = if wdp <= 0.0 {
+        //     1.0
+        // } else {
+        //     1.0f64.min(grad_norm.powi(3) / (self.radius * wdp))
+        // };
+        //
+        // let new_param = grad.scale(-tau * self.radius / grad_norm);
+        // let out = ArgminIterationData::new(new_param, 0.0);
+        // Ok(out)
+    }
+}
+
+impl<'a, T, H> ArgminTrustRegion for Dogleg<'a, T, H>
+where
+    T: Clone
+        + std::default::Default
+        + std::fmt::Debug
+        + ArgminWeightedDot<T, f64, H>
+        + ArgminNorm<f64>
+        + ArgminScale<f64>,
+    H: Clone + std::default::Default,
+{
+    // fn set_initial_parameter(&mut self, param: T) {
+    //     self.base.set_cur_param(param);
+    // }
+
+    fn set_radius(&mut self, radius: f64) {
+        self.radius = radius;
+    }
+
+    fn set_grad(&mut self, grad: T) {
+        self.base.set_cur_grad(grad);
+    }
+
+    fn set_hessian(&mut self, hessian: H) {
+        self.base.set_cur_hessian(hessian);
+    }
+}
