c9e40f8efcf59dbfeae3c1d58c1c00821c838b18
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-11-01 08:43:44 +0100

    Semiautomatic update of documentation

---
+++ argmin/solver/conjugategradient/nonlinear_cg/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["NonlinearConjugateGradient","Nonlinear Conjugate Gradient struct"]]});
--- argmin/solver/conjugategradient/sidebar-items.js
+++ argmin/solver/conjugategradient/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["conjugategradient","Conjugate gradient method # Conjugate Gradient Method"]]});
+initSidebarItems({"mod":[["beta","Beta update methods for nonlinear CG # Beta update methods"],["cg","Conjugate gradient method # Conjugate Gradient Method"],["nonlinear_cg","Nonlinear Conjugate gradient method # Nonlinear Conjugate Gradient Method"]]});
