7c3a3cce3fd0301d0be9d3fcb279a2b4132fb161
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-11-19 13:41:50 +0100

    Semiautomatic update of documentation

--- argmin/prelude/failure/_core/future/sidebar-items.js
+++ argmin/prelude/failure/_core/future/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"trait":[["Future","A future represents an asychronous computation."]]});
+initSidebarItems({"trait":[["Future","A future represents an asynchronous computation."]]});
--- argmin/sidebar-items.js
+++ argmin/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["prelude","Definition of all relevant traits # Re-export of all relevant traits"],["solver","solvers"],["testfunctions","testfunctions"]]});
+initSidebarItems({"mod":[["prelude","Definition of all relevant traits # Re-export of all relevant traits"],["solver","Solvers"],["testfunctions","Testfunctions"]]});
