900ae40bfd4a8a2a3277100bd03c6cf54b8ed9d0
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-11-25 07:18:17 +0100

    Semiautomatic update of documentation

--- argmin/solver/linesearch/morethuente/sidebar-items.js
+++ argmin/solver/linesearch/morethuente/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["MoreThuenteLineSearch","More-Thuente Line Search"]]});
+initSidebarItems({"struct":[["MoreThuenteLineSearch","The More-Thuente line search is a method to find a step length which obeys the strong Wolfe conditions."]]});
