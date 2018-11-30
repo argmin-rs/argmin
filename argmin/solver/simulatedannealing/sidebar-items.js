f1000d938a1903ebc82188d5cc70dfb4c846b25c
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-11-30 07:17:17 +0100

    Semiautomatic update of documentation

--- argmin/solver/simulatedannealing/sidebar-items.js
+++ argmin/solver/simulatedannealing/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["SATempFunc","Definition of built-in temperature functions for Simulated Annealing."]],"struct":[["SimulatedAnnealing","Simulated Annealing struct"]]});
+initSidebarItems({"enum":[["SATempFunc","Temperature functions for Simulated Annealing."]],"struct":[["SimulatedAnnealing","Simulated Annealing"]]});
