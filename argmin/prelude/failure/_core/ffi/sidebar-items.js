386c9c88519ae8ef0d8b41d5f3a133d8334f3a58
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-12-07 13:17:21 +0100

    Semiautomatic update of documentation

--- argmin/prelude/failure/_core/ffi/sidebar-items.js
+++ argmin/prelude/failure/_core/ffi/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["c_void","Equivalent to C's `void` type when used as a [pointer]."]]});
+initSidebarItems({"enum":[["c_void","Equivalent to C's `void` type when used as a [pointer]."]],"struct":[["VaList","A wrapper for a `va_list`"]]});
