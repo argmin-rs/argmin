c06f5ed8c3f03fea1c3a6b6a6b6c661792635032
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-03-14 12:23:08 +0100

    adapt writers example to new design

--- examples/writers.rs
+++ examples/writers.rs
@@ -14,7 +14,6 @@ use argmin::testfunctions::rosenbrock;
 use argmin_core::finitediff::*;
 use ndarray::{array, Array1, Array2};
 use serde::{Deserialize, Serialize};
-use std::sync::Arc;
 
 #[derive(Clone, Default, Serialize, Deserialize)]
 struct Rosenbrock {
@@ -52,28 +51,24 @@ fn run() -> Result<(), Error> {
     let solver = BFGS::new(init_hessian, linesearch);
 
     // Create writer
-    let mut writer = WriteToFile::new("params", "param");
-
-    // Only save every 3 iterations
-    writer.set_mode(WriterMode::Every(3));
-
-    // Set serializer to JSON
-    writer.set_serializer(WriteToFileSerializer::JSON);
+    let writer = WriteToFile::new("params", "param")
+        // Only save every 3 iterations
+        .mode(WriterMode::Every(3))
+        // Set serializer to JSON
+        .serializer(WriteToFileSerializer::JSON);
 
     // Create writer which only saves new best ones
-    let mut writer2 = WriteToFile::new("params", "best");
-
-    // Only save new best
-    writer2.set_mode(WriterMode::NewBest);
-
-    // Set serializer to JSON
-    writer2.set_serializer(WriteToFileSerializer::JSON);
+    let writer2 = WriteToFile::new("params", "best")
+        // Only save new best
+        .mode(WriterMode::NewBest)
+        // Set serializer to JSON
+        .serializer(WriteToFileSerializer::JSON);
 
     let res = Executor::new(cost, solver, init_param)
         .max_iters(10)
         .add_observer(ArgminSlogLogger::term())
-        .add_observer(Arc::new(writer))
-        .add_observer(Arc::new(writer2))
+        .add_observer(writer)
+        .add_observer(writer2)
         .run()?;
 
     // Wait a second (lets the logger flush everything before printing again)
