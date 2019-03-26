524ccee94c295a084d6112233181c9f2b9ce101f
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-03-26 06:56:53 +0100

    documentation

--- src/lib.rs
+++ src/lib.rs
@@ -453,6 +453,41 @@
 //!     }
 //! }
 //! ```
+//!
+//! # TODOs
+//!
+//!   * More optimization methods
+//!   * Automatic differentiation
+//!   * Parallelization
+//!   * Tests
+//!   * Evaluation on real problems
+//!   * Evaluation framework
+//!   * Documentation & Tutorials
+//!   * C interface
+//!   * Python wrapper
+//!   * Solver and problem definition via a config file
+//!
+//! Please open an [issue](https://github.com/argmin-rs/argmin/issues) if you want to contribute!
+//! Any help is appreciated!
+//!
+//! # License
+//!
+//! Licensed under either of
+//!
+//!   * Apache License, Version 2.0,
+//!     ([LICENSE-APACHE](https://github.com/argmin-rs/argmin/blob/master/LICENSE-APACHE) or
+//!     http://www.apache.org/licenses/LICENSE-2.0)
+//!   * MIT License ([LICENSE-MIT](https://github.com/argmin-rs/argmin/blob/master/LICENSE-MIT) or
+//!     http://opensource.org/licenses/MIT)
+//!
+//! at your option.
+//!
+//!
+//! ## Contribution
+//!
+//! Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion
+//! in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above,
+//! without any additional terms or conditions.
 
 #![warn(missing_docs)]
 #![allow(unused_attributes)]
