128209454b44122ffd8640d036aa69bf52fbc6c5
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-04-02 06:59:23 +0200

    derive Clone for Landweber

--- src/solver/landweber/mod.rs
+++ src/solver/landweber/mod.rs
@@ -32,7 +32,7 @@ use serde::{Deserialize, Serialize};
 /// [0] Landweber, L. (1951): An iteration formula for Fredholm integral equations of the first
 /// kind. Amer. J. Math. 73, 615–624
 /// [1] https://en.wikipedia.org/wiki/Landweber_iteration
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Landweber {
     /// omega
     omega: f64,
