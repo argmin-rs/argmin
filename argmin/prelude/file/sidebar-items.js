b68a5323f8027e8cbe0cc28a5c00955b4d60e4f5
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-04-23 06:54:21 +0200

    Semiautomatic update of documentation

--- argmin/prelude/file/sidebar-items.js
+++ argmin/prelude/file/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({});
+initSidebarItems({"enum":[["WriteToFileSerializer",""]],"struct":[["WriteToFile",""]]});
---
+++ argmin/solver/particleswarm/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Particle","A single particle"],["ParticleSwarm","Particle Swarm Optimization (PSO)"]],"trait":[["Position",""]]});
