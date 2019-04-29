9049c483354852ad25f7a7ea72bf3e47b0859de4
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-04-29 09:54:46 +0200

    added linesearch to Gauss-Newton method (still not tested)

--- src/solver/gaussnewton/mod.rs
+++ src/solver/gaussnewton/mod.rs
@@ -14,7 +14,7 @@ use crate::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::default::Default;
 
-/// Gauss-Newton method
+/// Gauss-Newton method (untested!)
 ///
 /// [Example](https://github.com/argmin-rs/argmin/blob/master/examples/gaussnewton.rs)
 ///
@@ -23,19 +23,24 @@ use std::default::Default;
 /// [0] Jorge Nocedal and Stephen J. Wright (2006). Numerical Optimization.
 /// Springer. ISBN 0-387-30303-0.
 #[derive(Serialize, Deserialize)]
-pub struct GaussNewton {
+pub struct GaussNewton<L> {
     /// gamma
     gamma: f64,
+    /// line search
+    linesearch: L,
 }
 
-impl GaussNewton {
+impl<L> GaussNewton<L> {
     /// Constructor
-    pub fn new() -> Self {
-        GaussNewton { gamma: 1.0 }
+    pub fn new(linesearch: L) -> Self {
+        GaussNewton {
+            gamma: 1.0,
+            linesearch,
+        }
     }
 
     /// set gamma
-    pub fn set_gamma(mut self, gamma: f64) -> Result<Self, Error> {
+    pub fn gamma(mut self, gamma: f64) -> Result<Self, Error> {
         if gamma <= 0.0 || gamma > 1.0 {
             return Err(ArgminError::InvalidParameter {
                 text: "Gauss-Newton: gamma must be in  (0, 1].".to_string(),
@@ -47,23 +52,27 @@ impl GaussNewton {
     }
 }
 
-impl Default for GaussNewton {
-    fn default() -> GaussNewton {
-        GaussNewton::new()
-    }
-}
+// impl Default for GaussNewton {
+//     fn default() -> GaussNewton {
+//         GaussNewton::new()
+//     }
+// }
 
-impl<O> Solver<O> for GaussNewton
+impl<O, L> Solver<O> for GaussNewton<L>
 where
     O: ArgminOp,
-    O::Param: ArgminScaledSub<O::Param, f64, O::Param>
+    O::Param: Default
+        + ArgminScaledSub<O::Param, f64, O::Param>
         + ArgminDot<O::Param, O::Param>
         + ArgminMul<f64, O::Param>,
+    O::Output: ArgminNorm<f64>,
     O::Jacobian: ArgminTranspose
         + ArgminInv<O::Jacobian>
         + ArgminDot<O::Jacobian, O::Jacobian>
         + ArgminDot<O::Output, O::Param>
         + ArgminDot<O::Param, O::Param>,
+    O::Hessian: Default,
+    L: Clone + ArgminLineSearch<O::Param> + Solver<OpWrapper<O>>,
 {
     const NAME: &'static str = "Gauss-Newton method";
 
@@ -84,9 +93,28 @@ where
             .dot(&jacobian.t().dot(&residuals))
             .mul(&1.0);
 
-        let new_param = param.scaled_sub(&self.gamma, &p.dot(&grad));
+        self.linesearch.set_search_direction(p);
 
-        Ok(ArgminIterData::new().param(new_param))
+        // TODO: Need to build another operator which does not return the residuals when calling
+        // `apply`, but instead returns the norm of the residuals. Otherwise this may not work....
+        let ArgminResult {
+            operator: line_op,
+            state:
+                IterState {
+                    param: new_param,
+                    cost: new_cost,
+                    ..
+                },
+        } = Executor::new(OpWrapper::new_from_op(&op), self.linesearch.clone(), param)
+            .grad(grad)
+            .cost(residuals.norm())
+            .ctrlc(false)
+            .run()?;
+
+        op.consume_op(line_op);
+        // let new_param = param.scaled_sub(&self.gamma, &p.dot(&grad));
+
+        Ok(ArgminIterData::new().param(new_param).cost(new_cost))
     }
 }
 
@@ -94,6 +122,10 @@ where
 mod tests {
     use super::*;
     use crate::send_sync_test;
+    use crate::solver::linesearch::MoreThuenteLineSearch;
 
-    send_sync_test!(gauss_newton_method, GaussNewton);
+    send_sync_test!(
+        gauss_newton_method,
+        GaussNewton<MoreThuenteLineSearch<Vec<f64>>>
+    );
 }
