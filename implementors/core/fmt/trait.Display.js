337db89550fc07355f859a81ceb413854fa2d094
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-12-18 10:52:42 +0100

    Semiautomatic update of documentation

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["argmin"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"argmin/solver/brent/struct.BrentError.html\" title=\"struct argmin::solver::brent::BrentError\">BrentError</a>",synthetic:false,types:["argmin::solver::brent::BrentError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
