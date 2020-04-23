43b481bad283d7c58fff9483378b3f6246a670f1
Stefan Kroboth <stefan.kroboth@gmail.com>
2020-04-23 06:26:01 +0200

    Semiautomatic update of documentation

---
+++ argmin/solver/goldensectionsearch/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["GoldenSectionSearch","Golden-section search"]]});
--- implementors/argmin/prelude/float/trait.FloatConst.js
+++ implementors/argmin/prelude/float/trait.FloatConst.js
@@ -1,3 +1,9 @@
 (function() {var implementors = {};
 implementors["argmin"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
