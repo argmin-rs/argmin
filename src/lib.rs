e4bc20b3364e827b476ae78898348fd4e9088949
Stefan Kroboth <stefan.kroboth@gmail.com>
2021-07-18 18:15:03 +0200

    update ndarray, ndarray-linalg, nalgebra, ndarray-rand, num-complex (#129)

--- src/lib.rs
+++ src/lib.rs
@@ -79,7 +79,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! argmin = "0.4.5"
+//! argmin = "0.4.6"
 //! ```
 //!
 //! ## Optional features (recommended)
@@ -88,7 +88,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! argmin = { version = "0.4.5", features = ["ctrlc", "ndarrayl", "nalgebral"] }
+//! argmin = { version = "0.4.6", features = ["ctrlc", "ndarrayl", "nalgebral"] }
 //! ```
 //!
 //! These may become default features in the future. Without these features compilation to
