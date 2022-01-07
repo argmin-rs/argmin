e7df170dd75a0fdcfaee86b2604e3c4736cc2823
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-01-07 08:28:02 +0100

    Semiautomatic update of documentation

--- argmin/solver/linesearch/sidebar-items.js
+++ argmin/solver/linesearch/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["backtracking","Backtracking line search algorithm"],["condition","Acceptance conditions"],["hagerzhang","Hager-Zhang line search algorithm"],["morethuente","More-Thuente line search algorithm"]]});
+initSidebarItems({"mod":[["backtracking","Backtracking line search algorithm"],["condition","Acceptance conditions"],["hagerzhang","Hager-Zhang line search algorithm"],["morethuente","More-Thuente line search algorithm TODO: Apparently it is missing stopping criteria!"]]});
