9f53e1f6806efa648c10e9ef38989cabcb56ee47
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-03-20 12:35:30 +0100

    Improved documentation of solver.rs

--- argmin/src/core/solver.rs
+++ argmin/src/core/solver.rs
@@ -7,23 +7,84 @@
 
 use crate::core::{Error, Problem, SerializeAlias, State, TerminationReason, KV};
 
-/// Solver
+/// The interface all solvers are required to implement.
 ///
-/// Every solver needs to implement this trait.
+/// Every solver needs to implement this trait in order to function with the `Executor`.
+/// It handles initialization ([`init`](`Solver::init`)), each iteration of the solver
+/// ([`next_iter`](`Solver::next_iter`)), and termination of the algorithm
+/// ([`terminate`](`Solver::terminate`) and [`terminate_internal`](`Solver::terminate_internal`)).
+/// Only `next_iter` is mandatory to implement, all others provide default implementations.
+///
+/// A `Solver` needs to be serializable.
+///
+/// # Example
+///
+/// ```
+/// use argmin::core::{
+///     ArgminFloat, Solver, IterState, CostFunction, Error, KV, Problem, TerminationReason
+/// };
+/// #[cfg(feature = "serde1")]
+/// use serde::{Deserialize, Serialize};
+///
+/// #[derive(Clone)]
+/// #[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+/// struct OptimizationAlgorithm {}
+///
+/// impl<O, P, G, J, H, F> Solver<O, IterState<P, G, J, H, F>> for OptimizationAlgorithm
+/// where
+///     O: CostFunction<Param = P, Output = F>,
+///     P: Clone,
+///     F: ArgminFloat
+/// {
+///     const NAME: &'static str = "OptimizationAlgorithm";
+///
+///     fn init(
+///         &mut self,
+///         problem: &mut Problem<O>,
+///         state: IterState<P, G, J, H, F>,
+///     ) -> Result<(IterState<P, G, J, H, F>, Option<KV>), Error> {
+///         // Initialize algorithm, update `state`.
+///         // Implementing this method is optional.
+///         Ok((state, None))
+///     }
+///
+///     fn next_iter(
+///         &mut self,
+///         problem: &mut Problem<O>,
+///         state: IterState<P, G, J, H, F>,
+///     ) -> Result<(IterState<P, G, J, H, F>, Option<KV>), Error> {
+///         // Compute single iteration of algorithm, update `state`.
+///         // Implementing this method is required.
+///         Ok((state, None))
+///     }
+///     
+///     fn terminate(&mut self, state: &IterState<P, G, J, H, F>) -> TerminationReason {
+///         // Check if stopping criteria are met.
+///         // Implementing this method is optional.
+///         TerminationReason::NotTerminated
+///     }
+/// }
+/// ```
 pub trait Solver<O, I: State>: SerializeAlias {
-    /// Name of the solver
-    const NAME: &'static str = "UNDEFINED";
+    /// Name of the solver. Mainly used in [Observers](`crate::core::Observe`).
+    const NAME: &'static str;
 
-    /// Computes one iteration of the algorithm.
-    fn next_iter(&mut self, problem: &mut Problem<O>, state: I) -> Result<(I, Option<KV>), Error>;
-
-    /// Initializes the algorithm
+    /// Initializes the algorithm.
     ///
-    /// This is executed before any iterations are performed. It can be used to perform
-    /// precomputations. The default implementation corresponds to doing nothing.
+    /// Executed before any iterations are performed and has access to the optimization problem
+    /// definition and the internal state of the solver.
+    /// Returns an updated `state` and optionally a `KV` which holds key-value pairs used in
+    /// [Observers](`crate::core::Observe`).
+    /// The default implementation returns the unaltered `state` and no `KV`.
     fn init(&mut self, _problem: &mut Problem<O>, state: I) -> Result<(I, Option<KV>), Error> {
         Ok((state, None))
     }
+
+    /// Computes a single iteration of the algorithm and has access to the optimization problem
+    /// definition and the internal state of the solver.
+    /// Returns an updated `state` and optionally a `KV` which holds key-value pairs used in
+    /// [Observers](`crate::core::Observe`).
+    fn next_iter(&mut self, problem: &mut Problem<O>, state: I) -> Result<(I, Option<KV>), Error>;
 
     /// Checks whether basic termination reasons apply.
     ///
@@ -31,9 +92,10 @@ pub trait Solver<O, I: State>: SerializeAlias {
     ///
     /// 1) algorithm was terminated somewhere else in the Executor
     /// 2) iteration count exceeds maximum number of iterations
-    /// 3) cost is lower than target cost
+    /// 3) cost is lower than or equal to the target cost
     ///
-    /// This can be overwritten in a `Solver` implementation; however it is not advised.
+    /// This can be overwritten; however it is not advised. It is recommended to implement other
+    /// stopping criteria via ([`terminate`](`Solver::terminate`).
     fn terminate_internal(&mut self, state: &I) -> TerminationReason {
         let solver_terminate = self.terminate(state);
         if solver_terminate.terminated() {
@@ -48,7 +110,10 @@ pub trait Solver<O, I: State>: SerializeAlias {
         TerminationReason::NotTerminated
     }
 
-    /// Checks whether the algorithm must be terminated
+    /// Used to implement stopping criteria, in particular criteria which are not covered by
+    /// ([`terminate_internal`](`Solver::terminate_internal`).
+    ///
+    /// This method has access to the internal state and returns an `TerminationReason`.
     fn terminate(&mut self, _state: &I) -> TerminationReason {
         TerminationReason::NotTerminated
     }
