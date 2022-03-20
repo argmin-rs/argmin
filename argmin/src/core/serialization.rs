4fd149bb1a8e9e8873ac4120cb9615ada98a0f4f
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-03-20 17:27:48 +0100

    Improved documentation of serialization.rs

--- argmin/src/core/serialization.rs
+++ argmin/src/core/serialization.rs
@@ -8,12 +8,16 @@
 #[cfg(feature = "serde1")]
 use serde::{de::DeserializeOwned, Serialize};
 
+/// Trait alias for `serde`s `Serialize`.
+///
 /// If the `serde1` feature is set, it acts as an alias for `Serialize` and is implemented for all
 /// types which implement `Serialize`. If `serde1` is not set, it will be an "empty" trait
 /// implemented for all types.
 #[cfg(feature = "serde1")]
 pub trait SerializeAlias: Serialize {}
 
+/// Trait alias for `serde`s `Serialize`.
+///
 /// If the `serde1` feature is set, it acts as an alias for `Serialize` and is implemented for all
 /// types which implement `Serialize`. If `serde1` is not set, it will be an "empty" trait
 /// implemented for all types.
@@ -22,14 +26,20 @@ pub trait SerializeAlias {}
 
 #[cfg(feature = "serde1")]
 impl<T> SerializeAlias for T where T: Serialize {}
+
 #[cfg(not(feature = "serde1"))]
 impl<T> SerializeAlias for T {}
 
+/// Trait alias for `serde`s `DeserializeOwned`.
+///
 /// If the `serde1` feature is set, it acts as an alias for `DeserializeOwned` and is implemented
 /// for all types which implement `DeserializeOwned`. If `serde1` is not set, it will be an "empty"
 /// trait implemented for all types.
 #[cfg(feature = "serde1")]
 pub trait DeserializeOwnedAlias: DeserializeOwned {}
+
+/// Trait alias for `serde`s `DeserializeOwned`.
+///
 /// If the `serde1` feature is set, it acts as an alias for `DeserializeOwned` and is implemented
 /// for all types which implement `DeserializeOwned`. If `serde1` is not set, it will be an "empty"
 /// trait implemented for all types.
@@ -38,5 +48,6 @@ pub trait DeserializeOwnedAlias {}
 
 #[cfg(feature = "serde1")]
 impl<T> DeserializeOwnedAlias for T where T: DeserializeOwned {}
+
 #[cfg(not(feature = "serde1"))]
 impl<T> DeserializeOwnedAlias for T {}
