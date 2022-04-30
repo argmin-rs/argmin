8de91326b2ef517cb7e70ef0a8bc5be728722585
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-04-30 20:30:50 +0200

    Cleanup of NewtonCG, better documentation and doc and unit tests
    Method `curvature_threshold` renamed to `with_curvature_threshold`.
    
    Hessian in subproblem is now stored as reference which avoids cloning of
    the Hessian.
    
    A missing initial parameter vector results in an Error.
    
    Gradient and Hessian are now taken from state if available. This allows
    users to provide Gradients and Hessians. If not availably they will be
    computed.

--- argmin/src/solver/newton/newton_cg.rs
+++ argmin/src/solver/newton/newton_cg.rs
@@ -18,10 +18,8 @@ use serde::{Deserialize, Serialize};
 
 /// # Newton-Conjugate-Gradient (Newton-CG) method
 ///
-/// The Newton-CG method (also called truncated Newton method) uses a modified CG to solve the
-/// Newton equations approximately. After a search direction is found, a line search is performed.
-///
-/// TODO: Stop when search direction is close to 0
+/// The Newton-CG method (also called truncated Newton method) uses a modified CG to approximately
+/// solve the Newton equations. After a search direction is found, a line search is performed.
 ///
 /// ## Reference
 ///
@@ -42,7 +40,15 @@ impl<L, F> NewtonCG<L, F>
 where
     F: ArgminFloat,
 {
-    /// Constructor
+    /// Construct a new instance of [`NewtonCG`]
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use argmin::solver::newton::NewtonCG;
+    /// # let linesearch = ();
+    /// let ncg: NewtonCG<_, f64> = NewtonCG::new(linesearch);
+    /// ```
     pub fn new(linesearch: L) -> Self {
         NewtonCG {
             linesearch,
@@ -52,18 +58,42 @@ where
     }
 
     /// Set curvature threshold
+    ///
+    /// Defaults to 0.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use argmin::solver::newton::NewtonCG;
+    /// # let linesearch = ();
+    /// let ncg: NewtonCG<_, f64> = NewtonCG::new(linesearch).with_curvature_threshold(1e-6);
+    /// ```
     #[must_use]
-    pub fn curvature_threshold(mut self, threshold: F) -> Self {
+    pub fn with_curvature_threshold(mut self, threshold: F) -> Self {
         self.curvature_threshold = threshold;
         self
     }
 
     /// Set tolerance for the stopping criterion based on cost difference
+    ///
+    /// Must be larger than 0 and defaults to EPSILON.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use argmin::solver::newton::NewtonCG;
+    /// # use argmin::core::Error;
+    /// # fn main() -> Result<(), Error> {
+    /// # let linesearch = ();
+    /// let ncg: NewtonCG<_, f64> = NewtonCG::new(linesearch).with_tolerance(1e-6)?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn with_tolerance(mut self, tol: F) -> Result<Self, Error> {
         if tol <= float!(0.0) {
             return Err(argmin_error!(
                 InvalidParameter,
-                "Newton-CG: tol must be positive."
+                "`NewtonCG`: tol must be > 0."
             ));
         }
         self.tol = tol;
@@ -95,27 +125,41 @@ where
         problem: &mut Problem<O>,
         mut state: IterState<P, G, (), H, F>,
     ) -> Result<(IterState<P, G, (), H, F>, Option<KV>), Error> {
-        let param = state.take_param().unwrap();
-        let grad = problem.gradient(&param)?;
-        let hessian = problem.hessian(&param)?;
+        let param = state.take_param().ok_or_else(argmin_error_closure!(
+            NotInitialized,
+            concat!(
+                "`NewtonCG` requires an initial parameter vector. ",
+                "Please provide an initial guess via `Executor`s `configure` method."
+            )
+        ))?;
+        let grad = state
+            .take_grad()
+            .map(Result::Ok)
+            .unwrap_or_else(|| problem.gradient(&param))?;
+        let hessian = state
+            .take_hessian()
+            .map(Result::Ok)
+            .unwrap_or_else(|| problem.hessian(&param))?;
 
         // Solve CG subproblem
-        let cg_problem: CGSubProblem<P, H> = CGSubProblem::new(hessian.clone());
-        let mut cg_problem = Problem::new(cg_problem);
+        let mut cg_problem = Problem::new(CGSubProblem::new(&hessian));
 
         let mut x_p = param.zero_like();
-        let mut x: P = param.zero_like();
+        let mut x = param.zero_like();
         let mut cg = ConjugateGradient::new(grad.mul(&(float!(-1.0))));
 
-        let cg_state = IterState::new().param(x_p.clone());
-        let (mut cg_state, _) = cg.init(&mut cg_problem, cg_state)?;
-        let grad_norm = grad.norm();
+        let (mut cg_state, _) = cg.init(&mut cg_problem, IterState::new().param(x_p.clone()))?;
+
+        let grad_norm_factor = float!(0.5).min(grad.norm().sqrt()) * grad.norm();
+
         for iter in 0.. {
-            let (state_tmp, _) = cg.next_iter(&mut cg_problem, cg_state)?;
-            cg_state = state_tmp;
+            (cg_state, _) = cg.next_iter(&mut cg_problem, cg_state)?;
+
             let cost = cg_state.get_cost();
+
             x = cg_state.take_param().unwrap();
             let p = cg.get_prev_p()?;
+
             let curvature = p.dot(&hessian.dot(p));
             if curvature <= self.curvature_threshold {
                 if iter == 0 {
@@ -125,14 +169,17 @@ where
                 }
                 break;
             }
-            if cost <= float!(0.5).min(grad_norm.sqrt()) * grad_norm {
+
+            if cost <= grad_norm_factor {
                 break;
             }
+
             cg_state = cg_state.param(x.clone()).cost(cost);
             x_p = x.clone();
         }
 
         // perform line search
+        // TODO: Should the algorithm stop when search direction is close to 0?
         self.linesearch.search_direction(x);
 
         let line_cost = state.get_cost();
@@ -143,7 +190,7 @@ where
             state: mut linesearch_state,
             ..
         } = Executor::new(problem.take_problem().unwrap(), self.linesearch.clone())
-            .configure(|config| config.param(param).grad(grad).cost(line_cost))
+            .configure(|state| state.param(param).grad(grad).cost(line_cost))
             .ctrlc(false)
             .run()?;
 
@@ -159,7 +206,7 @@ where
 
     fn terminate(&mut self, state: &IterState<P, G, (), H, F>) -> TerminationReason {
         if (state.get_cost() - state.get_prev_cost()).abs() < self.tol {
-            TerminationReason::NoChangeInCost
+            TerminationReason::TargetToleranceReached
         } else {
             TerminationReason::NotTerminated
         }
@@ -167,15 +214,14 @@ where
 }
 
 #[derive(Clone)]
-#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
-struct CGSubProblem<P, H> {
-    hessian: H,
+struct CGSubProblem<'a, P, H> {
+    hessian: &'a H,
     phantom: std::marker::PhantomData<P>,
 }
 
-impl<T, H> CGSubProblem<T, H> {
-    /// constructor
-    pub fn new(hessian: H) -> Self {
+impl<'a, P, H> CGSubProblem<'a, P, H> {
+    /// Constructor
+    fn new(hessian: &'a H) -> Self {
         CGSubProblem {
             hessian,
             phantom: std::marker::PhantomData,
@@ -183,10 +229,9 @@ impl<T, H> CGSubProblem<T, H> {
     }
 }
 
-impl<P, H> Operator for CGSubProblem<P, H>
+impl<'a, P, H> Operator for CGSubProblem<'a, P, H>
 where
     H: ArgminDot<P, P>,
-    P: Clone + SerializeAlias + DeserializeOwnedAlias,
 {
     type Param = P;
     type Output = P;
@@ -199,6 +244,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::core::{test_utils::TestProblem, ArgminError};
     use crate::solver::linesearch::MoreThuenteLineSearch;
     use crate::test_trait_impl;
 
@@ -221,4 +267,72 @@ mod tests {
 
         assert!((t - tol1).abs() < std::f64::EPSILON);
     }
+
+    #[test]
+    fn test_new() {
+        #[derive(Eq, PartialEq, Debug, Copy, Clone)]
+        struct LineSearch {}
+        let ls = LineSearch {};
+        let ncg: NewtonCG<_, f64> = NewtonCG::new(ls);
+        let NewtonCG {
+            linesearch,
+            curvature_threshold,
+            tol,
+        } = ncg;
+        assert_eq!(linesearch, ls);
+        assert_eq!(curvature_threshold.to_ne_bytes(), 0.0f64.to_ne_bytes());
+        assert_eq!(tol.to_ne_bytes(), f64::EPSILON.to_ne_bytes());
+    }
+
+    #[test]
+    fn test_with_curvature_threshold() {
+        #[derive(Eq, PartialEq, Debug, Copy, Clone)]
+        struct LineSearch {}
+        let ls = LineSearch {};
+        let ncg: NewtonCG<_, f64> = NewtonCG::new(ls).with_curvature_threshold(1e-6);
+        let NewtonCG {
+            linesearch,
+            curvature_threshold,
+            tol,
+        } = ncg;
+        assert_eq!(linesearch, ls);
+        assert_eq!(curvature_threshold.to_ne_bytes(), 1e-6f64.to_ne_bytes());
+        assert_eq!(tol.to_ne_bytes(), f64::EPSILON.to_ne_bytes());
+    }
+
+    #[test]
+    fn test_with_tolerance() {
+        let ls = ();
+        for tolerance in [f64::EPSILON, 1.0, 10.0, 100.0] {
+            let ncg: NewtonCG<_, f64> = NewtonCG::new(ls).with_tolerance(tolerance).unwrap();
+            assert_eq!(ncg.tol.to_ne_bytes(), tolerance.to_ne_bytes());
+        }
+
+        for tolerance in [-f64::EPSILON, 0.0, -1.0] {
+            let res = NewtonCG::new(ls).with_tolerance(tolerance);
+            assert_error!(
+                res,
+                ArgminError,
+                "Invalid parameter: \"`NewtonCG`: tol must be > 0.\""
+            );
+        }
+    }
+
+    #[test]
+    fn test_next_iter_param_not_initialized() {
+        use crate::solver::linesearch::{condition::ArmijoCondition, BacktrackingLineSearch};
+        let ls = BacktrackingLineSearch::new(ArmijoCondition::new(0.9f64).unwrap());
+        let mut ncg: NewtonCG<_, f64> = NewtonCG::new(ls);
+        let res = ncg.next_iter(&mut Problem::new(TestProblem::new()), IterState::new());
+        assert_error!(
+            res,
+            ArgminError,
+            concat!(
+                "Not initialized: \"`NewtonCG` requires an initial parameter vector. ",
+                "Please provide an initial guess via `Executor`s `configure` method.\""
+            )
+        );
+    }
+
+    // TODO: Test next_iter.
 }
