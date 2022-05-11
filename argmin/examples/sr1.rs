9b47d4cabcd2c1caa2f1c485ebac04bf3a467941
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-05-11 22:17:28 +0200

    Cleanup of SR1, added doc and unit tests, better documentation
    Documentation was improved and tolerance parameters are now explained.
    
    Method `r` was renamed to `with_denominator_factor`.
    
    Values provided to the `with_*` methods are now checked to be within the
    allowed bounds.
    
    Initial Hessian was removed from the SR1 struct and is instead taken
    from the state.
    
    Users can now provide initial gradient and cost. Those will be
    calculated if not provided.
    
    Doc and unit tests were added.

--- argmin/examples/sr1.rs
+++ argmin/examples/sr1.rs
@@ -7,53 +7,56 @@
 
 use argmin::core::observers::{ObserverMode, SlogLogger};
 use argmin::core::{CostFunction, Error, Executor, Gradient};
+// use argmin::solver::linesearch::HagerZhangLineSearch;
 use argmin::solver::linesearch::MoreThuenteLineSearch;
 use argmin::solver::quasinewton::SR1;
-use argmin_testfunctions::rosenbrock;
+use argmin_testfunctions::styblinski_tang;
 use finitediff::FiniteDiff;
 use ndarray::{array, Array1, Array2};
 
-struct Rosenbrock {
-    a: f64,
-    b: f64,
-}
+struct StyblinskiTang {}
 
-impl CostFunction for Rosenbrock {
+impl CostFunction for StyblinskiTang {
     type Param = Array1<f64>;
     type Output = f64;
 
     fn cost(&self, p: &Self::Param) -> Result<Self::Output, Error> {
-        Ok(rosenbrock(&p.to_vec(), self.a, self.b))
+        Ok(styblinski_tang(&p.to_vec()))
     }
 }
-impl Gradient for Rosenbrock {
+impl Gradient for StyblinskiTang {
     type Param = Array1<f64>;
     type Gradient = Array1<f64>;
 
     fn gradient(&self, p: &Self::Param) -> Result<Self::Param, Error> {
-        Ok((*p).forward_diff(&|x| rosenbrock(&x.to_vec(), self.a, self.b)))
+        Ok((*p).forward_diff(&|x| styblinski_tang(&x.to_vec())))
     }
 }
 
 fn run() -> Result<(), Error> {
     // Define cost function
-    let cost = Rosenbrock { a: 1.0, b: 100.0 };
+    let cost = StyblinskiTang {};
 
     // Define initial parameter vector
-    // let init_param: Array1<f64> = array![-1.2, 1.0];
-    // let init_hessian: Array2<f64> = Array2::eye(2);
-    let init_param: Array1<f64> = array![-1.2, 1.0, -10.0, 2.0, 3.0, 2.0, 4.0, 10.0];
+    // let init_param: Array1<f64> = array![-1.2, 1.0, -5.0, 2.0, 3.0, 2.0, 4.0, 5.0];
+    let init_param: Array1<f64> = array![5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0];
     let init_hessian: Array2<f64> = Array2::eye(8);
 
     // set up a line search
     let linesearch = MoreThuenteLineSearch::new().with_c(1e-4, 0.9)?;
+    // let linesearch = HagerZhangLineSearch::new();
 
     // Set up solver
-    let solver = SR1::new(init_hessian, linesearch);
+    let solver = SR1::new(linesearch);
 
     // Run solver
     let res = Executor::new(cost, solver)
-        .configure(|state| state.param(init_param).max_iters(1000))
+        .configure(|state| {
+            state
+                .param(init_param)
+                .inv_hessian(init_hessian)
+                .max_iters(1000)
+        })
         .add_observer(SlogLogger::term(), ObserverMode::Always)
         .run()?;
 
