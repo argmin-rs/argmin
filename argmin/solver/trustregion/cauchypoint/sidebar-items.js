aa6dda1e7ed7b1b41937b1956bddbc064f2fa1ce
stefan-k <stefan-k@users.noreply.github.com>
2022-06-18 14:51:16 +0000

    deploy: 0d7c9e89a0ee7144323e63891be0b2ba7a18222e

--- argmin/solver/trustregion/cauchypoint/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["CauchyPoint","The Cauchy point is the minimum of the quadratic approximation of the cost function within the trust region along the direction given by the first derivative."]]});
