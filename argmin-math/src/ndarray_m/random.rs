c874876e8e9938c32476f1d7958df1e13a77552d
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-12-09 08:58:12 +0100

    Explicitly implement ArgminRandom for types (ndarray)

--- argmin-math/src/ndarray_m/random.rs
+++ argmin-math/src/ndarray_m/random.rs
@@ -5,55 +5,65 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use crate::ArgminRandom;
-use rand::distributions::uniform::SampleUniform;
 use rand::Rng;
 
-impl<T> ArgminRandom for ndarray::Array1<T>
-where
-    T: SampleUniform + std::cmp::PartialOrd + Clone,
-{
-    fn rand_from_range(min: &Self, max: &Self) -> ndarray::Array1<T> {
-        assert!(!min.is_empty());
-        assert_eq!(min.len(), max.len());
+use crate::ArgminRandom;
 
-        let mut rng = rand::thread_rng();
+macro_rules! make_random {
+    ($t:ty) => {
+        impl ArgminRandom for ndarray::Array1<$t> {
+            fn rand_from_range(min: &Self, max: &Self) -> ndarray::Array1<$t> {
+                assert!(!min.is_empty());
+                assert_eq!(min.len(), max.len());
 
-        ndarray::Array1::from_iter(min.iter().zip(max.iter()).map(|(a, b)| {
-            // Do not require a < b:
+                let mut rng = rand::thread_rng();
 
-            if a == b {
-                a.clone()
-            } else if a < b {
-                rng.gen_range(a.clone()..b.clone())
-            } else {
-                rng.gen_range(b.clone()..a.clone())
+                ndarray::Array1::from_iter(min.iter().zip(max.iter()).map(|(a, b)| {
+                    // Do not require a < b:
+
+                    // We do want to know if a and b are *exactly* the same.
+                    #[allow(clippy::float_cmp)]
+                    if a == b {
+                        a.clone()
+                    } else if a < b {
+                        rng.gen_range(a.clone()..b.clone())
+                    } else {
+                        rng.gen_range(b.clone()..a.clone())
+                    }
+                }))
             }
-        }))
-    }
+        }
+
+        impl ArgminRandom for ndarray::Array2<$t> {
+            fn rand_from_range(min: &Self, max: &Self) -> ndarray::Array2<$t> {
+                assert!(!min.is_empty());
+                assert_eq!(min.raw_dim(), max.raw_dim());
+
+                let mut rng = rand::thread_rng();
+
+                ndarray::Array2::from_shape_fn(min.raw_dim(), |(i, j)| {
+                    let a = min.get((i, j)).unwrap();
+                    let b = max.get((i, j)).unwrap();
+
+                    // We do want to know if a and b are *exactly* the same.
+                    #[allow(clippy::float_cmp)]
+                    if a == b {
+                        a.clone()
+                    } else if a < b {
+                        rng.gen_range(a.clone()..b.clone())
+                    } else {
+                        rng.gen_range(b.clone()..a.clone())
+                    }
+                })
+            }
+        }
+    };
 }
 
-impl<T> ArgminRandom for ndarray::Array2<T>
-where
-    T: SampleUniform + std::cmp::PartialOrd + Clone,
-{
-    fn rand_from_range(min: &Self, max: &Self) -> ndarray::Array2<T> {
-        assert!(!min.is_empty());
-        assert_eq!(min.raw_dim(), max.raw_dim());
-
-        let mut rng = rand::thread_rng();
-
-        ndarray::Array2::from_shape_fn(min.raw_dim(), |(i, j)| {
-            let a = min.get((i, j)).unwrap();
-            let b = max.get((i, j)).unwrap();
-
-            if a == b {
-                a.clone()
-            } else if a < b {
-                rng.gen_range(a.clone()..b.clone())
-            } else {
-                rng.gen_range(b.clone()..a.clone())
-            }
-        })
-    }
-}
+make_random!(isize);
+make_random!(i8);
+make_random!(i16);
+make_random!(i32);
+make_random!(i64);
+make_random!(f32);
+make_random!(f64);
