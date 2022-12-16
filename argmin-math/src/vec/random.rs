303984bc1db3f0f40458e1c49064ff13a5331a7e
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-12-16 19:27:25 +0100

    Extended ArgminRandom impl for Vec<T> and Vec<Vec<T> and added tests

--- argmin-math/src/vec/random.rs
+++ argmin-math/src/vec/random.rs
@@ -6,32 +6,115 @@
 // copied, modified, or distributed except according to those terms.
 
 use crate::ArgminRandom;
-use rand::distributions::uniform::SampleUniform;
 use rand::Rng;
 
-impl<T> ArgminRandom for Vec<T>
-where
-    T: SampleUniform + std::cmp::PartialOrd + Clone,
-{
-    fn rand_from_range(min: &Self, max: &Self) -> Vec<T> {
-        assert!(!min.is_empty());
-        assert_eq!(min.len(), max.len());
+macro_rules! make_random {
+    ($t:ty) => {
+        impl ArgminRandom for Vec<$t> {
+            fn rand_from_range(min: &Self, max: &Self) -> Vec<$t> {
+                assert!(!min.is_empty());
+                assert_eq!(min.len(), max.len());
 
-        let mut rng = rand::thread_rng();
+                let mut rng = rand::thread_rng();
 
-        min.iter()
-            .zip(max.iter())
-            .map(|(a, b)| {
-                // Do not require a < b:
+                min.iter()
+                    .zip(max.iter())
+                    .map(|(a, b)| {
+                        // Do not require a < b:
 
-                if a == b {
-                    a.clone()
-                } else if a < b {
-                    rng.gen_range(a.clone()..b.clone())
-                } else {
-                    rng.gen_range(b.clone()..a.clone())
+                        // We do want to know if a and b are *exactly* the same.
+                        #[allow(clippy::float_cmp)]
+                        if a == b {
+                            a.clone()
+                        } else if a < b {
+                            rng.gen_range(a.clone()..b.clone())
+                        } else {
+                            rng.gen_range(b.clone()..a.clone())
+                        }
+                    })
+                    .collect()
+            }
+        }
+
+        impl ArgminRandom for Vec<Vec<$t>> {
+            fn rand_from_range(min: &Self, max: &Self) -> Vec<Vec<$t>> {
+                assert!(!min.is_empty());
+                assert_eq!(min.len(), max.len());
+                min.iter()
+                    .zip(max.iter())
+                    .map(|(a, b)| Vec::<$t>::rand_from_range(a, b))
+                    .collect()
+            }
+        }
+    };
+}
+
+make_random!(f32);
+make_random!(f64);
+make_random!(i8);
+make_random!(i16);
+make_random!(i32);
+make_random!(i64);
+make_random!(u8);
+make_random!(u16);
+make_random!(u32);
+make_random!(u64);
+make_random!(isize);
+make_random!(usize);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use paste::item;
+
+    macro_rules! make_test {
+        ($t:ty) => {
+            item! {
+                #[test]
+                fn [<test_random_vec_ $t>]() {
+                    let a = vec![1 as $t, 2 as $t, 4 as $t];
+                    let b = vec![2 as $t, 3 as $t, 5 as $t];
+                    let random = Vec::<$t>::rand_from_range(&a, &b);
+                    for i in 0..3usize {
+                        assert!(random[i] >= a[i]);
+                        assert!(random[i] <= b[i]);
+                    }
                 }
-            })
-            .collect()
+            }
+
+            item! {
+                #[test]
+                fn [<test_random_mat_ $t>]() {
+                    let a = vec![
+                        vec![1 as $t, 2 as $t, 4 as $t],
+                        vec![2 as $t, 3 as $t, 5 as $t]
+                    ];
+                    let b = vec![
+                        vec![2 as $t, 3 as $t, 5 as $t],
+                        vec![3 as $t, 4 as $t, 6 as $t]
+                    ];
+                    let random = Vec::<Vec<$t>>::rand_from_range(&a, &b);
+                    for i in 0..3 {
+                        for j in 0..2 {
+                            assert!(random[j][i] >= a[j][i]);
+                            assert!(random[j][i] <= b[j][i]);
+                        }
+                    }
+                }
+            }
+        };
     }
+
+    make_test!(i8);
+    make_test!(u8);
+    make_test!(i16);
+    make_test!(u16);
+    make_test!(i32);
+    make_test!(u32);
+    make_test!(i64);
+    make_test!(u64);
+    make_test!(isize);
+    make_test!(usize);
+    make_test!(f32);
+    make_test!(f64);
 }
