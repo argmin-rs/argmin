43a484668d5ca62e7a152f007a6f5bf78145cbe4
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-12-18 08:18:21 +0100

    Added tests for Array2 type in ndarray signum impl

--- argmin-math/src/ndarray_m/signum.rs
+++ argmin-math/src/ndarray_m/signum.rs
@@ -5,8 +5,6 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-// TODO: Tests for Array2 impl
-
 use crate::ArgminSignum;
 use ndarray::{Array1, Array2};
 use num_complex::Complex;
@@ -87,13 +85,14 @@ make_signum_complex!(Complex<f64>);
 #[cfg(test)]
 mod tests {
     use super::*;
+    use ndarray::array;
     use paste::item;
 
     macro_rules! make_test {
         ($t:ty) => {
             item! {
                 #[test]
-                fn [<test_signum_complex_ndarray_ $t>]() {
+                fn [<test_signum_complex_array1_ $t>]() {
                     let x = Array1::from(vec![
                         Complex::new(1 as $t, 2 as $t),
                         Complex::new(4 as $t, -3 as $t),
@@ -117,13 +116,76 @@ mod tests {
 
             item! {
                 #[test]
-                fn [<test_signum_ndarray_ $t>]() {
+                fn [<test_signum_array1_ $t>]() {
                     let x = Array1::from(vec![1 as $t, -4 as $t, 8 as $t]);
                     let y = Array1::from(vec![1 as $t, -1 as $t, 1 as $t]);
                     let res = <Array1<$t> as ArgminSignum>::signum(x);
                     for i in 0..3 {
                         let diff = (y[i] - res[i]).abs() as f64;
                         assert!(diff < std::f64::EPSILON);
+                    }
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_signum_complex_array2_ $t>]() {
+                    let x = array![
+                        [
+                            Complex::new(1 as $t, 2 as $t),
+                            Complex::new(4 as $t, -3 as $t),
+                            Complex::new(-8 as $t, 4 as $t),
+                            Complex::new(-8 as $t, -1 as $t),
+                        ],
+                        [
+                            Complex::new(-1 as $t, -2 as $t),
+                            Complex::new(4 as $t, 3 as $t),
+                            Complex::new(-8 as $t, -4 as $t),
+                            Complex::new(8 as $t, -1 as $t),
+                        ]
+                    ];
+                    let y = array![
+                        [
+                            Complex::new(1 as $t, 1 as $t),
+                            Complex::new(1 as $t, -1 as $t),
+                            Complex::new(-1 as $t, 1 as $t),
+                            Complex::new(-1 as $t, -1 as $t),
+                        ],
+                        [
+                            Complex::new(-1 as $t, -1 as $t),
+                            Complex::new(1 as $t, 1 as $t),
+                            Complex::new(-1 as $t, -1 as $t),
+                            Complex::new(1 as $t, -1 as $t),
+                        ]
+                    ];
+                    let res = <Array2<Complex<$t>> as ArgminSignum>::signum(x);
+                    for j in 0..2 {
+                        for i in 0..4 {
+                            let tmp = y[(j, i)] - res[(j, i)];
+                            let norm = ((tmp.re * tmp.re + tmp.im * tmp.im) as f64).sqrt();
+                            assert!(norm < std::f64::EPSILON);
+                        }
+                    }
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_signum_array2_ $t>]() {
+                    let x = array![
+                        [1 as $t, -4 as $t, 8 as $t],
+                        [-2 as $t, 2 as $t, -8 as $t]
+                    ];
+                    let y = array![
+                        [1 as $t, -1 as $t, 1 as $t],
+                        [-1 as $t, 1 as $t, -1 as $t]
+                    ];
+                    let res = <Array2<$t> as ArgminSignum>::signum(x);
+                    for j in 0..2 {
+                        for i in 0..3 {
+                            let diff = (y[(j, i)] - res[(j, i)]).abs() as f64;
+                            assert!(diff < std::f64::EPSILON);
+                        }
                     }
                 }
             }
