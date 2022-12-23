b1f6cb5a40e5d9a566d811f2d5d42fa8e6cab632
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-12-23 08:28:45 +0100

    Extended ArgminL1Norm impl for vec types and added tests for complex types

--- argmin-math/src/vec/l1norm.rs
+++ argmin-math/src/vec/l1norm.rs
@@ -8,7 +8,7 @@
 use crate::ArgminL1Norm;
 use num_complex::Complex;
 
-macro_rules! make_l1norm_float {
+macro_rules! make_l1norm {
     ($t:ty) => {
         impl ArgminL1Norm<$t> for Vec<$t> {
             #[inline]
@@ -19,11 +19,11 @@ macro_rules! make_l1norm_float {
     };
 }
 
-macro_rules! make_l1norm_complex_float {
-    ($t:ty) => {
-        impl ArgminL1Norm<Complex<$t>> for Vec<Complex<$t>> {
+macro_rules! make_l1norm_complex {
+    ($i: ty, $t:ty) => {
+        impl ArgminL1Norm<$t> for Vec<$i> {
             #[inline]
-            fn l1_norm(&self) -> Complex<$t> {
+            fn l1_norm(&self) -> $t {
                 self.iter().map(|a| a.l1_norm()).sum::<$t>().into()
             }
         }
@@ -41,31 +41,30 @@ macro_rules! make_l1norm_unsigned {
     };
 }
 
-macro_rules! make_l1norm_integer {
-    ($t:ty) => {
-        impl ArgminL1Norm<$t> for Vec<$t> {
-            #[inline]
-            fn l1_norm(&self) -> $t {
-                self.iter().map(|a| a.abs()).sum()
-            }
-        }
-    };
-}
-
-make_l1norm_integer!(isize);
-make_l1norm_unsigned!(usize);
-make_l1norm_integer!(i8);
-make_l1norm_integer!(i16);
-make_l1norm_integer!(i32);
-make_l1norm_integer!(i64);
 make_l1norm_unsigned!(u8);
 make_l1norm_unsigned!(u16);
 make_l1norm_unsigned!(u32);
 make_l1norm_unsigned!(u64);
-make_l1norm_float!(f32);
-make_l1norm_float!(f64);
-make_l1norm_complex_float!(f32);
-make_l1norm_complex_float!(f64);
+make_l1norm_unsigned!(usize);
+make_l1norm!(i8);
+make_l1norm!(i16);
+make_l1norm!(i32);
+make_l1norm!(i64);
+make_l1norm!(isize);
+make_l1norm!(f32);
+make_l1norm!(f64);
+make_l1norm_complex!(Complex<i8>, i8);
+make_l1norm_complex!(Complex<i16>, i16);
+make_l1norm_complex!(Complex<i32>, i32);
+make_l1norm_complex!(Complex<i64>, i64);
+make_l1norm_complex!(Complex<isize>, isize);
+make_l1norm_complex!(Complex<u8>, u8);
+make_l1norm_complex!(Complex<u16>, u16);
+make_l1norm_complex!(Complex<u32>, u32);
+make_l1norm_complex!(Complex<u64>, u64);
+make_l1norm_complex!(Complex<usize>, usize);
+make_l1norm_complex!(Complex<f32>, f32);
+make_l1norm_complex!(Complex<f64>, f64);
 
 #[cfg(test)]
 mod tests {
@@ -83,6 +82,16 @@ mod tests {
                     assert!(((target - res) as f64).abs() < std::f64::EPSILON);
                 }
             }
+
+            item! {
+                #[test]
+                fn [<test_norm_complex_ $t>]() {
+                    let a = vec![Complex::new(4 as $t, 2 as $t), Complex::new(3 as $t, 4 as $t)];
+                    let res = <Vec<Complex<$t>> as ArgminL1Norm<$t>>::l1_norm(&a);
+                    let target = a[0].l1_norm() + a[1].l1_norm();
+                    assert!(((target - res) as f64).abs() < std::f64::EPSILON);
+                }
+            }
         };
     }
 
@@ -94,6 +103,16 @@ mod tests {
                     let a = vec![-4 as $t, -3 as $t];
                     let res = <Vec<$t> as ArgminL1Norm<$t>>::l1_norm(&a);
                     let target = 7 as $t;
+                    assert!(((target - res) as f64).abs() < std::f64::EPSILON);
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_norm_signed_complex_ $t>]() {
+                    let a = vec![Complex::new(-4 as $t, -2 as $t), Complex::new(-3 as $t, -4 as $t)];
+                    let res = <Vec<Complex<$t>> as ArgminL1Norm<$t>>::l1_norm(&a);
+                    let target = a[0].l1_norm() + a[1].l1_norm();
                     assert!(((target - res) as f64).abs() < std::f64::EPSILON);
                 }
             }
