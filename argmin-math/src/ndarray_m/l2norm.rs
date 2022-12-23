71d0032dc28e381b83453cc0daee0880d297ef30
Stefan Kroboth <stefan.kroboth@gmail.com>
2022-12-23 13:49:47 +0100

    Extended ArgminL2Norm impl for ndarray types and added tests for complex types

--- argmin-math/src/ndarray_m/l2norm.rs
+++ argmin-math/src/ndarray_m/l2norm.rs
@@ -21,28 +21,6 @@ macro_rules! make_norm_float {
     };
 }
 
-macro_rules! make_norm_complex_float {
-    ($t:ty) => {
-        impl ArgminL2Norm<Complex<$t>> for Array1<Complex<$t>> {
-            #[inline]
-            fn l2_norm(&self) -> Complex<$t> {
-                self.iter().map(|a| a.powf(2.0)).sum::<Complex<$t>>().sqrt()
-            }
-        }
-
-        impl ArgminL2Norm<$t> for Array1<Complex<$t>> {
-            #[inline]
-            fn l2_norm(&self) -> $t {
-                self.iter()
-                    .map(|a| a.powf(2.0))
-                    .sum::<Complex<$t>>()
-                    .sqrt()
-                    .norm()
-            }
-        }
-    };
-}
-
 macro_rules! make_norm_integer {
     ($t:ty) => {
         impl ArgminL2Norm<$t> for Array1<$t> {
@@ -54,20 +32,52 @@ macro_rules! make_norm_integer {
     };
 }
 
-make_norm_integer!(isize);
-make_norm_integer!(usize);
+macro_rules! make_norm_complex {
+    ($i: ty, $t:ty) => {
+        impl ArgminL2Norm<$t> for Array1<$i> {
+            #[inline]
+            fn l2_norm(&self) -> $t {
+                self.iter().map(|a| a.norm_sqr()).sum::<$t>().sqrt()
+            }
+        }
+    };
+}
+
+macro_rules! make_norm_unsigned {
+    ($t:ty) => {
+        impl ArgminL2Norm<$t> for Array1<$t> {
+            #[inline]
+            fn l2_norm(&self) -> $t {
+                self.iter().map(|a| a.pow(2)).sum::<$t>().sqrt()
+            }
+        }
+    };
+}
+
+make_norm_unsigned!(u8);
+make_norm_unsigned!(u16);
+make_norm_unsigned!(u32);
+make_norm_unsigned!(u64);
+make_norm_unsigned!(usize);
 make_norm_integer!(i8);
 make_norm_integer!(i16);
 make_norm_integer!(i32);
 make_norm_integer!(i64);
-make_norm_integer!(u8);
-make_norm_integer!(u16);
-make_norm_integer!(u32);
-make_norm_integer!(u64);
+make_norm_integer!(isize);
 make_norm_float!(f32);
 make_norm_float!(f64);
-make_norm_complex_float!(f32);
-make_norm_complex_float!(f64);
+make_norm_complex!(Complex<i8>, i8);
+make_norm_complex!(Complex<i16>, i16);
+make_norm_complex!(Complex<i32>, i32);
+make_norm_complex!(Complex<i64>, i64);
+make_norm_complex!(Complex<isize>, isize);
+make_norm_complex!(Complex<u8>, u8);
+make_norm_complex!(Complex<u16>, u16);
+make_norm_complex!(Complex<u32>, u32);
+make_norm_complex!(Complex<u64>, u64);
+make_norm_complex!(Complex<usize>, usize);
+make_norm_complex!(Complex<f32>, f32);
+make_norm_complex!(Complex<f64>, f64);
 
 #[cfg(test)]
 mod tests {
@@ -86,6 +96,16 @@ mod tests {
                     assert!(((target - res) as f64).abs() < std::f64::EPSILON);
                 }
             }
+
+            item! {
+                #[test]
+                fn [<test_norm_complex_ $t>]() {
+                    let a = array![Complex::new(4 as $t, 2 as $t), Complex::new(3 as $t, 4 as $t)];
+                    let res = <Array1<Complex<$t>> as ArgminL2Norm<$t>>::l2_norm(&a);
+                    let target = (a[0].norm_sqr() + a[1].norm_sqr()).sqrt();
+                    assert!(((target - res) as f64).abs() < std::f64::EPSILON);
+                }
+            }
         };
     }
 
@@ -97,6 +117,16 @@ mod tests {
                     let a = array![-4 as $t, -3 as $t];
                     let res = <Array1<$t> as ArgminL2Norm<$t>>::l2_norm(&a);
                     let target = 5 as $t;
+                    assert!(((target - res) as f64).abs() < std::f64::EPSILON);
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_norm_signed_complex_ $t>]() {
+                    let a = array![Complex::new(-4 as $t, -2 as $t), Complex::new(-3 as $t, -4 as $t)];
+                    let res = <Array1<Complex<$t>> as ArgminL2Norm<$t>>::l2_norm(&a);
+                    let target = (a[0].norm_sqr() + a[1].norm_sqr()).sqrt();
                     assert!(((target - res) as f64).abs() < std::f64::EPSILON);
                 }
             }
