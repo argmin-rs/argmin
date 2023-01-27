e0d83f03bdb91fd754beb8052c40447e8b3b230c
stefan-k <stefan-k@users.noreply.github.com>
2023-01-27 20:44:53 +0000

    deploy: 5aa8547dce77f52560697441326a2d04194b7cdb

--- implementors/core/error/trait.Error.js
+++ implementors/core/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"argmin":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"argmin/core/enum.ArgminError.html\" title=\"enum argmin::core::ArgminError\">ArgminError</a>"]]
+"argmin":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"argmin/core/enum.ArgminError.html\" title=\"enum argmin::core::ArgminError\">ArgminError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
