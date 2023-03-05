ad465dc8680d59d52f69df2021d5ddeeb2aaaa70
Stefan Kroboth <stefan.kroboth@gmail.com>
2023-03-05 12:47:29 +0100

    Documentation of plotter

--- tools/plotter/src/lib.rs
+++ tools/plotter/src/lib.rs
@@ -5,6 +5,42 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+//! # argmin plotter
+//!
+//! The argmin plotter is a dedicated GUI program which displays the progress of an optimization
+//! run. The data is fed to the plotter by the `argmin-observer-plotter`.
+//!
+//! ## Installation
+//!
+//! ```bash
+//! cargo install argmin-plotter
+//! ```
+//!
+//! ## Library
+//!
+//! This crate can also be used as a library and exposes the [`Message`] type used to encode data
+//! sent to the plotter and [`DEFAULT_PORT`] which defines the default port used by the plotter.
+//!
+//! # License
+//!
+//! Licensed under either of
+//!
+//!   * Apache License, Version 2.0,
+//!     ([LICENSE-APACHE](https://github.com/argmin-rs/argmin/blob/main/LICENSE-APACHE) or
+//!     <http://www.apache.org/licenses/LICENSE-2.0>)
+//!   * MIT License ([LICENSE-MIT](https://github.com/argmin-rs/argmin/blob/main/LICENSE-MIT) or
+//!     <http://opensource.org/licenses/MIT>)
+//!
+//! at your option.
+//!
+//! ## Contribution
+//!
+//! Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion
+//! in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above,
+//! without any additional terms or conditions.
+
+#![warn(missing_docs)]
+
 mod message;
 
 pub use message::Message;
--- tools/plotter/src/message.rs
+++ tools/plotter/src/message.rs
@@ -13,52 +13,84 @@ use bytes::{Bytes, BytesMut};
 use serde::{Deserialize, Serialize};
 use time::Duration;
 
-#[derive(Clone, Serialize, Deserialize, Debug)]
+/// Enum used to encode information sent to plotter.
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 pub enum Message {
+    /// Register a new run
     NewRun {
+        /// Name of the run
         name: String,
+        /// Name of the solver
         solver: String,
+        /// Maximum number of iterations
         max_iter: u64,
+        /// Target cost function value
         target_cost: f64,
+        /// Initial parameter vector
         init_param: Option<Vec<f64>>,
+        /// Solver-specific settings (returned by the `init` method of the `Solver` trait)
         settings: KV,
+        /// Preselected metrics
         selected: HashSet<String>,
     },
+    /// A set of metrics samples sent after an iteration
     Samples {
+        /// Name of the run
         name: String,
+        /// Current iteration
         iter: u64,
+        /// Time needed for this iteration
         time: Duration,
+        /// Current termination_status
         termination_status: TerminationStatus,
+        /// Solver-specific metrics
         kv: KV,
     },
+    /// Function evaluation counts (Cost function, gradient, Hessian, ...)
     FuncCounts {
+        /// Name of the run
         name: String,
+        /// Current iteration
         iter: u64,
+        /// Function counts
         kv: HashMap<String, u64>,
     },
+    /// Parameter vector
     Param {
+        /// Name of the run
         name: String,
+        /// Current iteration
         iter: u64,
+        /// Current parameter vector
         param: Vec<f64>,
     },
+    /// Current best parameter vector
     BestParam {
+        /// Name of the run
         name: String,
+        /// Current iteration
         iter: u64,
+        /// Current best parameter vector
         param: Vec<f64>,
     },
+    /// Termination
     Termination {
+        /// Name of the run
         name: String,
+        /// Termination status
         termination_status: TerminationStatus,
     },
 }
 
 impl Message {
+    /// Serialize message
     #[allow(unused)]
     pub fn pack(&self) -> Result<Bytes, Error> {
         let buf = rmp_serde::encode::to_vec(&self)?;
         Ok(Bytes::from(buf))
     }
 
+    /// Deserialize message
     #[allow(unused)]
     pub fn unpack(buf: &BytesMut) -> Result<Self, Error> {
         Ok(rmp_serde::from_slice::<Message>(buf)?)
