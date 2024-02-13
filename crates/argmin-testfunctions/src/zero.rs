61324b825a1ae61a1f556621b0d7168e383d60f3
Stefan Kroboth <stefan.kroboth@gmail.com>
2024-02-13 16:41:02 +0100

    Added derivative and Hessian for zero test function

--- crates/argmin-testfunctions/src/zero.rs
+++ crates/argmin-testfunctions/src/zero.rs
@@ -32,6 +32,37 @@ where
     vec![T::from_f64(0.0).unwrap(); param.len()]
 }
 
+/// Derivative of zero test function (const version)
+///
+/// Always returns an array with the length of param, full of `0.0`. This is only for performance
+/// tests.
+pub fn zero_derivative_const<const N: usize, T>(_param: &[T; N]) -> [T; N]
+where
+    T: Float + FromPrimitive,
+{
+    [T::from_f64(0.0).unwrap(); N]
+}
+
+/// Hessian of zero test function
+///
+/// Always returns a matrix with size NxN, full of `0.0`. This is only for performance tests.
+pub fn zero_hessian<T>(param: &[T]) -> Vec<Vec<T>>
+where
+    T: Float + FromPrimitive,
+{
+    vec![vec![T::from_f64(0.0).unwrap(); param.len()]; param.len()]
+}
+
+/// Hessian of zero test function (const version)
+///
+/// Always returns a matrix with size NxN, full of `0.0`. This is only for performance tests.
+pub fn zero_hessian_const<const N: usize, T>(_param: &[T; N]) -> [[T; N]; N]
+where
+    T: Float + FromPrimitive,
+{
+    [[T::from_f64(0.0).unwrap(); N]; N]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,6 +88,43 @@ mod tests {
 
         zero_derivative(&[0.0_f32, 0.0, 23.0, 28.0])
             .iter()
+            .map(|x| assert_eq!(x.to_ne_bytes(), 0.0_f32.to_ne_bytes()))
+            .count();
+
+        zero_derivative_const(&[0.0_f64, 0.0, 23.0, 28.0])
+            .iter()
+            .map(|x| assert_eq!(x.to_ne_bytes(), 0.0_f64.to_ne_bytes()))
+            .count();
+
+        zero_derivative_const(&[0.0_f32, 0.0, 23.0, 28.0])
+            .iter()
+            .map(|x| assert_eq!(x.to_ne_bytes(), 0.0_f32.to_ne_bytes()))
+            .count();
+    }
+
+    #[test]
+    fn test_zero_hessian() {
+        zero_hessian(&[0.0_f64, 0.0, 23.0, 28.0])
+            .iter()
+            .flatten()
+            .map(|x| assert_eq!(x.to_ne_bytes(), 0.0_f64.to_ne_bytes()))
+            .count();
+
+        zero_hessian(&[0.0_f32, 0.0, 23.0, 28.0])
+            .iter()
+            .flatten()
+            .map(|x| assert_eq!(x.to_ne_bytes(), 0.0_f32.to_ne_bytes()))
+            .count();
+
+        zero_hessian_const(&[0.0_f64, 0.0, 23.0, 28.0])
+            .iter()
+            .flatten()
+            .map(|x| assert_eq!(x.to_ne_bytes(), 0.0_f64.to_ne_bytes()))
+            .count();
+
+        zero_hessian_const(&[0.0_f32, 0.0, 23.0, 28.0])
+            .iter()
+            .flatten()
             .map(|x| assert_eq!(x.to_ne_bytes(), 0.0_f32.to_ne_bytes()))
             .count();
     }
