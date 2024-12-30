2a56af4c426269aaf8b8ffded99993b623233c7b
geo-ant <54497890+geo-ant@users.noreply.github.com>
2024-12-30 18:48:48 +0100

    I think I understand zipped now... kinda

--- crates/argmin-math/src/faer_m/minmax.rs
+++ crates/argmin-math/src/faer_m/minmax.rs
@@ -1,36 +1,36 @@
-use faer::{mat, unzipped, zipped, Entity, Mat};
+use faer::{mat, unzipped, zipped, Entity, Mat, SimpleEntity};
 
 use crate::ArgminMinMax;
 
-impl<'a, E: Entity + PartialOrd> ArgminMinMax for Mat<E> {
+impl<'a, E: SimpleEntity + PartialOrd> ArgminMinMax for Mat<E> {
     #[inline]
     fn max(a: &Self, b: &Self) -> Self {
-        let mut res = Mat::<E>::zeros(a.nrows(), a.ncols());
-        faer::zipped_rw!(res.as_mut(), a.as_ref(), b.as_ref()).for_each(
-            |faer::unzipped!(mut res, a, b)| {
-                let aa = a.read();
-                let bb = b.read();
-                //@note(geo-ant) directly cribbed from the nalgebra implementation
-                // will have the same problems with NaN values.
-                res.write(if aa > bb { aa } else { bb });
-            },
-        );
-        res
+        faer::zipped!(a, b).map(|faer::unzipped!(a, b)| {
+            let aa = *a;
+            let bb = *b;
+            //@note(geo-ant) directly cribbed from the nalgebra implementation
+            // will have the same problems with NaN values.
+            if aa > bb {
+                aa
+            } else {
+                bb
+            }
+        })
     }
 
     #[inline]
     fn min(a: &Mat<E>, b: &Mat<E>) -> Mat<E> {
-        let mut res = Mat::<E>::zeros(a.nrows(), a.ncols());
-        faer::zipped_rw!(res.as_mut(), a.as_ref(), b.as_ref()).for_each(
-            |faer::unzipped!(mut res, a, b)| {
-                let aa = a.read();
-                let bb = b.read();
-                //@note(geo-ant) directly cribbed from the nalgebra implementation
-                // will have the same problems with NaN values.
-                res.write(if aa < bb { aa } else { bb });
-            },
-        );
-        res
+        faer::zipped!(a, b).map(|faer::unzipped!(a, b)| {
+            let aa = *a;
+            let bb = *b;
+            //@note(geo-ant) directly cribbed from the nalgebra implementation
+            // will have the same problems with NaN values.
+            if aa < bb {
+                aa
+            } else {
+                bb
+            }
+        })
     }
 }
 
