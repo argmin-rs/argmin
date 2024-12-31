3b6e17698d8f3ef0f897e38a045a2fe772941eac
geo-ant <54497890+geo-ant@users.noreply.github.com>
2024-12-31 11:27:02 +0100

    more tests

--- crates/argmin-math/src/faer_m/test_helper.rs
+++ crates/argmin-math/src/faer_m/test_helper.rs
@@ -1,6 +1,6 @@
-use faer::{mat::from_column_major_slice_generic, Conjugate, Mat, SimpleEntity};
+use faer::{mat::from_column_major_slice_generic, zipped, Conjugate, Mat, SimpleEntity};
 
 /// create a column vector (in Nx1 matrix form) from a Vec instance
-pub fn column_vector_from_vec<E: SimpleEntity + Conjugate<Canonical = E>>(vec: Vec<E>) -> Mat<E> {
-    from_column_major_slice_generic::<E, usize, usize>(vec.as_slice(), vec.len(), 1).to_owned()
+pub fn column_vector_from_vec<E: SimpleEntity>(vec: Vec<E>) -> Mat<E> {
+    Mat::<E>::from_fn(vec.len(), 1, |ir, _ic| vec[ir])
 }
