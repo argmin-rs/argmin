d14faf72f3a0e57fa94e31f3ee7cb76e3aa2915f
geo-ant <54497890+geo-ant@users.noreply.github.com>
2024-12-31 11:27:02 +0100

    more tests

--- crates/argmin-math/src/faer_m/signum.rs
+++ crates/argmin-math/src/faer_m/signum.rs
@@ -52,15 +52,57 @@ where
     R: Shape,
     C: Shape,
 {
+    #[inline]
     fn signum(self) -> Self {
         zipped_rw!(self).map(|unzipped!(elem)| elem.read().signum_internal())
     }
 }
 
 #[cfg(test)]
-mod test {
-    #[test]
-    fn add_tests() {
-        todo!()
+mod tests {
+    use super::super::test_helper::*;
+    use super::*;
+    use approx::assert_relative_eq;
+    use faer::mat;
+    use faer::Mat;
+    use paste::item;
+
+    macro_rules! make_test {
+        ($t:ty) => {
+            item! {
+                #[test]
+                fn [<test_signum_ $t>]() {
+                    let a = column_vector_from_vec(vec![3 as $t, -4 as $t, -8 as $t]);
+                    let b = column_vector_from_vec(vec![1 as $t, -1 as $t, -1 as $t]);
+                    let res = <_ as ArgminSignum>::signum(a);
+                    for i in 0..3 {
+                        assert_relative_eq!(b[(i,0)], res[(i,0)], epsilon = $t::EPSILON);
+                    }
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_signum_scalar_mat_2_ $t>]() {
+                    let b = mat![
+                        [3 as $t, -4 as $t, 8 as $t],
+                        [-2 as $t, -5 as $t, 9 as $t]
+                    ];
+                    let target = mat![
+                        [1 as $t, -1 as $t, 1 as $t],
+                        [-1 as $t, -1 as $t, 1 as $t]
+                    ];
+                    let res = b.signum();
+                    for i in 0..3 {
+                        for j in 0..2 {
+                            assert_relative_eq!(target[(j, i)], res[(j, i)], epsilon = $t::EPSILON);
+                        }
+                    }
+                }
+            }
+        };
     }
+
+    make_test!(f32);
+    make_test!(f64);
 }
