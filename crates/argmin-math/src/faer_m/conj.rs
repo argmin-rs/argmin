6ebd34e16fe6cd63f163181775ca6e5ea19e38b1
geo-ant <54497890+geo-ant@users.noreply.github.com>
2025-01-03 10:31:22 +0100

    fix conj and add tests

--- crates/argmin-math/src/faer_m/conj.rs
+++ crates/argmin-math/src/faer_m/conj.rs
@@ -1,36 +1,68 @@
 use crate::ArgminConj;
-use faer::{mat::AsMatRef, reborrow::ReborrowMut, Conjugate, Entity, Mat, MatMut, MatRef};
+use faer::{
+    mat::AsMatRef, reborrow::ReborrowMut, unzipped, Conjugate, Entity, Mat, MatMut, MatRef,
+    SimpleEntity,
+};
+use num_complex::ComplexFloat;
 
-impl<E: Entity + Conjugate<Conj = E>> ArgminConj for MatRef<'_, E> {
-    fn conj(&self) -> Self {
-        self.conjugate()
-    }
-}
-
-impl<E: Entity + Conjugate<Canonical = E, Conj = E>> ArgminConj for Mat<E> {
+impl<E: Entity + num_complex::ComplexFloat> ArgminConj for Mat<E> {
     #[inline]
     fn conj(&self) -> Self {
-        self.as_mat_ref().conj().to_owned()
+        //@note(geo-ant): we can't directly use the `conjugate()' function
+        // on the MatRef struct since it's not guaranteed to return matrix same type.
+        // Thus, we implement the conjugation using the num-complex trait manually
+        faer::zipped_rw!(self).map(|unzipped!(this)| ComplexFloat::conj(this.read()))
     }
 }
 
 #[cfg(test)]
-mod test {
-    use crate::ArgminConj;
-    use faer::{
-        mat::{AsMatMut, AsMatRef},
-        Mat,
-    };
+mod tests {
+    use super::super::test_helper::*;
+    use super::*;
+    use approx::assert_relative_eq;
+    use faer::linalg::entity::complex_split::ComplexConj;
+    use num_complex::Complex;
+    use paste::item;
 
-    #[test]
-    fn more_tests() {
-        todo!()
+    macro_rules! make_test {
+        ($t:ty) => {
+            item! {
+                #[test]
+                fn [<test_conj_complex_faer_ $t>]() {
+                        let a : Mat<Complex<$t>> = vector3_new(
+                        Complex::new(1 as $t, 2 as $t),
+                        Complex::new(4 as $t, -3 as $t),
+                        Complex::new(8 as $t, 0 as $t)
+                    );
+                    let b = vector3_new(
+                        Complex::new(1 as $t, -2 as $t),
+                        Complex::new(4 as $t, 3 as $t),
+                        Complex::new(8 as $t, 0 as $t)
+                    );
+                    let res: Mat<_> = <Mat<Complex<$t>> as ArgminConj>::conj(&a);
+                    assert_eq!(res.nrows(),3);
+                    assert_eq!(res.ncols(),1);
+                    for i in 0..3 {
+                        assert_relative_eq!(b.read(i,0).re(), res.read(i,0).re(), epsilon = $t::EPSILON);
+                        assert_relative_eq!(b.read(i,0).im(), res.read(i,0).im(), epsilon = $t::EPSILON);
+                    }
+                }
+            }
+
+            item! {
+                #[test]
+                fn [<test_conj_faer_ $t>]() {
+                    let a = vector3_new(1 as $t, 4 as $t, 8 as $t);
+                    let b = vector3_new(1 as $t, 4 as $t, 8 as $t);
+                    let res = <_ as ArgminConj>::conj(&a);
+                    for i in 0..3 {
+                        assert_relative_eq!(b[(i,0)], res[(i,0)], epsilon = $t::EPSILON);
+                    }
+                }
+            }
+        };
     }
 
-    #[test]
-    fn test_conj() {
-        let mat = Mat::<f64>::zeros(3, 2);
-        let expected = mat.conjugate();
-        assert_eq!(expected, <_ as ArgminConj>::conj(&mat.as_mat_ref()))
-    }
+    make_test!(f32);
+    make_test!(f64);
 }
