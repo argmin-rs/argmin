c15a294f0350c2504954081e7d3e313bc2860a69
stefan-k <stefan-k@users.noreply.github.com>
2025-03-22 14:55:52 +0000

    deploy: b35808a12abbf1e5ed4047a1e408cfae997a0063

--- trait.impl/core/cmp/trait.Ord.js
+++ trait.impl/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["argmin",[["impl&lt;O, S, I: <a class=\"trait\" href=\"argmin/core/trait.State.html\" title=\"trait argmin::core::State\">State</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"argmin/core/struct.OptimizationResult.html\" title=\"struct argmin::core::OptimizationResult\">OptimizationResult</a>&lt;O, S, I&gt;"]]]]);
+    var implementors = Object.fromEntries([["argmin",[["impl&lt;O, S, I: <a class=\"trait\" href=\"argmin/core/trait.State.html\" title=\"trait argmin::core::State\">State</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"argmin/core/struct.OptimizationResult.html\" title=\"struct argmin::core::OptimizationResult\">OptimizationResult</a>&lt;O, S, I&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/iter/traits/collect/trait.FromIterator.js
+++ trait.impl/core/iter/traits/collect/trait.FromIterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["argmin",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;(&amp;'static <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.0/std/primitive.str.html\">str</a>, <a class=\"enum\" href=\"argmin/core/enum.KvValue.html\" title=\"enum argmin::core::KvValue\">KvValue</a>)&gt; for <a class=\"struct\" href=\"argmin/core/struct.KV.html\" title=\"struct argmin::core::KV\">KV</a>"]]]]);
+    var implementors = Object.fromEntries([["argmin",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;(&amp;'static <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.1/std/primitive.str.html\">str</a>, <a class=\"enum\" href=\"argmin/core/enum.KvValue.html\" title=\"enum argmin::core::KvValue\">KvValue</a>)&gt; for <a class=\"struct\" href=\"argmin/core/struct.KV.html\" title=\"struct argmin::core::KV\">KV</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
