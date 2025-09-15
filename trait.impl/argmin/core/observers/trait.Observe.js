7f2648473f263124aafe75198c199fdb3b3a8ef6
stefan-k <stefan-k@users.noreply.github.com>
2025-09-15 20:06:07 +0000

    deploy: 25d19231e0ce83d46b2066d66a0f0c64d9078e6a

--- trait.impl/argmin/core/observers/trait.Observe.js
+++ trait.impl/argmin/core/observers/trait.Observe.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["argmin",[]],["argmin_observer_paramwriter",[["impl&lt;I&gt; Observe&lt;I&gt; for <a class=\"struct\" href=\"argmin_observer_paramwriter/struct.ParamWriter.html\" title=\"struct argmin_observer_paramwriter::ParamWriter\">ParamWriter</a><div class=\"where\">where\n    I: State,\n    &lt;I as State&gt;::Param: <a class=\"trait\" href=\"https://docs.rs/serde/1.0.221/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a>,</div>"]]],["argmin_observer_slog",[["impl&lt;I&gt; Observe&lt;I&gt; for <a class=\"struct\" href=\"argmin_observer_slog/struct.SlogLogger.html\" title=\"struct argmin_observer_slog::SlogLogger\">SlogLogger</a><div class=\"where\">where\n    I: State,</div>"]]]]);
+    var implementors = Object.fromEntries([["argmin",[]],["argmin_observer_paramwriter",[["impl&lt;I&gt; Observe&lt;I&gt; for <a class=\"struct\" href=\"argmin_observer_paramwriter/struct.ParamWriter.html\" title=\"struct argmin_observer_paramwriter::ParamWriter\">ParamWriter</a><div class=\"where\">where\n    I: State,\n    &lt;I as State&gt;::Param: <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.224/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a>,</div>"]]],["argmin_observer_slog",[["impl&lt;I&gt; Observe&lt;I&gt; for <a class=\"struct\" href=\"argmin_observer_slog/struct.SlogLogger.html\" title=\"struct argmin_observer_slog::SlogLogger\">SlogLogger</a><div class=\"where\">where\n    I: State,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[13,459,251]}
+//{"start":57,"fragment_lengths":[13,464,251]}
