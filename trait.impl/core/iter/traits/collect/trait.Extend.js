4c11be20cbee5b87f43d1547ace9915a47ee22dd
stefan-k <stefan-k@users.noreply.github.com>
2025-09-20 18:56:25 +0000

    deploy: 822f18b0cbf0ff5361d5ff3eca13b52b6c71662d

--- trait.impl/core/iter/traits/collect/trait.Extend.js
+++ trait.impl/core/iter/traits/collect/trait.Extend.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["argmin",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/iter/traits/collect/trait.Extend.html\" title=\"trait core::iter::traits::collect::Extend\">Extend</a>&lt;(&amp;'static <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.89.0/std/primitive.str.html\">str</a>, <a class=\"enum\" href=\"argmin/core/enum.KvValue.html\" title=\"enum argmin::core::KvValue\">KvValue</a>)&gt; for <a class=\"struct\" href=\"argmin/core/struct.KV.html\" title=\"struct argmin::core::KV\">KV</a>"]]]]);
+    var implementors = Object.fromEntries([["argmin",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/iter/traits/collect/trait.Extend.html\" title=\"trait core::iter::traits::collect::Extend\">Extend</a>&lt;(&amp;'static <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.90.0/std/primitive.str.html\">str</a>, <a class=\"enum\" href=\"argmin/core/enum.KvValue.html\" title=\"enum argmin::core::KvValue\">KvValue</a>)&gt; for <a class=\"struct\" href=\"argmin/core/struct.KV.html\" title=\"struct argmin::core::KV\">KV</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
